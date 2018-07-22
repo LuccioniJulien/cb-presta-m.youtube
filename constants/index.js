9104151cb400c6b3ebd1a1c61f9d932709c616b7
Unknown <julien.luccioni@protonmail.com>
2018-07-22 10:18:00 +0200

    Add Constant

---
+++ constants/index.js
@@ -0,0 +1,13 @@
+export const CONFIG = {
+  STORAGE:{
+    AVAILABLE_REGIONS:'@AVAIBLE_REGION',
+    CURRENT_REGION:'@CURRENT_REGION',
+    FAV:'@FAV'
+  },
+  YOUTUBE:{
+    BASE_URL:'https://www.googleapis.com/youtube/v3',
+    API_KEY:'AIzaSyDq_JV_7kIBn5KcL0obvJGbcyqkHteq9HU',
+    DEFAULT_REGION:'FR',
+    DEFAULT_NB_RESULT: 10
+  }
+}
